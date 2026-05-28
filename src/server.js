9a2611cd708b2e25866dce87ac1337543a9e045d
gonewiththesakura-creator <backup@local.invalid>
2026-05-28 17:52:30 +0800

    backup: pre-change 20260528-175230

--- src/server.js
+++ src/server.js
@@ -190,7 +190,7 @@ app.use(helmet({
   }
 }));
 app.use(cors({ origin: false }));
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: '25mb' }));
 app.use((req, res, next) => {
   const startedAt = Date.now();
   res.on('finish', () => recordAccessLog(req, res, startedAt));
